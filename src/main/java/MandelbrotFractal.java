14c5866ce6c8298a0be0af6a4e9a012f00d9b38f
Mikhail Semenov <mikhail.semenov@uno-soft.ru>
2024-11-14 16:00:30 +0300

    Done

--- src/main/java/MandelbrotFractal.java
+++ src/main/java/MandelbrotFractal.java
@@ -1,31 +1,73 @@
+import java.awt.image.BufferedImage;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import javax.swing.*;
+
 public class MandelbrotFractal {
+    public static final int WIDTH = 1920;
+    public static final int HEIGHT = 1056;
+    private static final int[] PALETTE = {
+        0x421e0f, 0x19071a, 0x09012f, 0x040449,
+        0x000764, 0x0c2c8a, 0x1852b1, 0x397DD1,
+        0x86b5e5, 0xd3ecf8, 0xf1e9bf, 0xf8c95f,
+        0xffaa00, 0xcc8000, 0x995700, 0x6a3403
+    };
+    private static final BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+    private static final JFrame frame = new JFrame("Множество Мандельброта");
 
     public static void main(String[] args) {
-        int width = 1600;
-        int height = 800;
-        int maxIterations = 100_000;
-        double zoom = 100;
-        double xShift = 0;
-        double yShift = 0;
-
-        for (int yPixel = 0; yPixel < height; yPixel++) {
-            StringBuilder row = new StringBuilder();
-            for (int xPixel = 0; xPixel < width; xPixel++) {
-                double zx = 0;
-                double zy = 0;
-                double cX = (xPixel - width / 2) / zoom + xShift;
-                double cY = (yPixel - height / 2) / zoom + yShift;
-                int iter = maxIterations;
-                while (zx * zx + zy * zy < 4 && iter > 0) {
-                    double tmp = zx * zx - zy * zy + cX;
-                    zy = 2.0 * zx * zy + cY;
-                    zx = tmp;
-                    iter--;
-                }
-                char colorChar = (iter == 0) ? '*' : ' ';
-                row.append(colorChar);
-            }
-            System.out.println(row);
-        }
+        JLabel label = new JLabel(new ImageIcon(image));
+        frame.add(label);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setExtendedState(frame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //sequential(10000, 1.0 / 512, -1.0, -2.0);
+        parallel(10000, 1.0 / 512, -1.0, -2.0);
     }
+    
+    public static long sequential(int maxIterations, double zoom, double top, double left) {
+        long startTime = System.currentTimeMillis();
+        for (int yPixel = 0; yPixel < HEIGHT; yPixel++) {
+            drawRow(yPixel, maxIterations, zoom, top, left);
+        }
+        return System.currentTimeMillis() - startTime;
+    }
+    
+    public static long parallel(int maxIterations, double zoom, double top, double left) {
+        long startTime = System.currentTimeMillis();
+        try (ExecutorService executor = Executors.newFixedThreadPool(4)) {
+            for (int yPixel = 0; yPixel < HEIGHT; yPixel++) {
+                final int y = yPixel;
+                executor.execute(() -> {
+                    drawRow(y, maxIterations, zoom, top, left);
+                });
+            }
+            executor.shutdown();
+        }
+        return System.currentTimeMillis() - startTime;
+    }
+    
+    private static void drawRow(int y, int maxIterations, double zoom, double top, double left) {
+        double ci = y * zoom + top;
+        for (int xPixel = 0; xPixel < WIDTH; xPixel++) {
+            double cr = xPixel * zoom + left;
+            double zr = 0.0;
+            double zi = 0.0;
+            int color = 0;
+            for (int iteration = 0; iteration < maxIterations; ++iteration) {
+                double zr2 = zr * zr;
+                double zi2 = zi * zi;
+                if (zr2 + zi2 >= 4) {
+                    color = PALETTE[iteration & 15];
+                    break;
+                }
+                zi = 2.0 * zr * zi + ci;
+                zr = zr2 - zi2 + cr;
+            }
+            image.setRGB(xPixel, y, color);
+        }
+        // Обновление фрейма после завершения вычислений для каждой строки
+        frame.repaint();
+    } 
 }
--- src/test/java/MandelbrotFractalTest.java
+++ src/test/java/MandelbrotFractalTest.java
@@ -1,8 +1,141 @@
+import java.awt.*;
+import java.io.*;
+import java.util.*;
+import java.util.List;
+import javax.swing.*;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.ChartUtils;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.block.BlockBorder;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.chart.title.TextTitle;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 import org.junit.jupiter.api.Test;
 
 public class MandelbrotFractalTest {
-    @Test
-    public void testMandelbrotFractal() {
+    public static final int ITERATIONS = 1000;
+    public static final double ZOOM = 1.0 / 512;
+    public static double TOP = -1.0;
+    public static double LEFT = -2.0;
 
+    @Test
+    public void testMandelbrotFractalIteration() throws IOException, InterruptedException {
+        List<Number> iterationMatrix = List.of(10, 100, 1000, 5000, 10000);
+        List<Long> sequentialResults = new ArrayList<>();
+        List<Long> parallelResults = new ArrayList<>();
+        for (Number nIterations : iterationMatrix) {
+            sequentialResults.add(MandelbrotFractal.sequential((int) nIterations, ZOOM, TOP, LEFT));
+            parallelResults.add(MandelbrotFractal.parallel((int) nIterations, ZOOM, TOP, LEFT));
+        }
+        XYSeriesCollection dataset = getDataset(iterationMatrix, sequentialResults, parallelResults);
+
+        JFreeChart chart = ChartFactory.createXYLineChart(
+            "Зависимость времени выполнения от кол-ва итераций",
+            "Кол-во итераций",
+            "Время (ms)",
+            dataset,
+            PlotOrientation.VERTICAL,
+            true,
+            true,
+            false
+        );
+        XYPlot plot = chart.getXYPlot();
+        var renderer = new XYLineAndShapeRenderer();
+        renderer.setSeriesPaint(0, Color.RED);
+        renderer.setSeriesStroke(0, new BasicStroke(2.0f));
+
+        renderer.setSeriesPaint(1, Color.BLUE);
+        renderer.setSeriesStroke(1, new BasicStroke(2.0f));
+        plot.setRenderer(renderer);
+        plot.setBackgroundPaint(Color.white);
+        plot.setRangeGridlinesVisible(false);
+        plot.setDomainGridlinesVisible(false);
+
+        chart.getLegend().setFrame(BlockBorder.NONE);
+        chart.setTitle(new TextTitle("Зависимость времени выполнения от кол-ва итераций", new Font("Serif", Font.BOLD, 18)));
+        ChartUtils.saveChartAsPNG(new File("charts/iteration_chart.png"), chart, 1080, 720);
+        
+        JFrame frame = new JFrame("Mandelbrot Fractal");
+        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+        chartPanel.setBackground(Color.white);
+        frame.add(chartPanel);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        while (true) {
+            Thread.sleep(1000);
+        }
+    }
+
+    @Test
+    public void testMandelbrotFractalZoom() throws IOException, InterruptedException {
+        List<Number> zoomMatrix = List.of(1.0 / 1024, 1.0 / 512, 1.0 / 256, 1.0 / 128, 1.0 / 64);
+        List<Long> sequentialResults = new ArrayList<>();
+        List<Long> parallelResults = new ArrayList<>();
+        for (Number zoomValue : zoomMatrix) {
+            sequentialResults.add(MandelbrotFractal.sequential(ITERATIONS, (double) zoomValue, TOP, LEFT));
+            parallelResults.add(MandelbrotFractal.parallel(ITERATIONS, (double) zoomValue, TOP, LEFT));
+        }
+        XYSeriesCollection dataset = getDataset(zoomMatrix, sequentialResults, parallelResults);
+
+        JFreeChart chart = ChartFactory.createXYLineChart(
+            "Зависимость времени выполнения от зума прорисовки",
+            "Зум",
+            "Время (ms)",
+            dataset,
+            PlotOrientation.VERTICAL,
+            true,
+            true,
+            false
+        );
+        XYPlot plot = chart.getXYPlot();
+        var renderer = new XYLineAndShapeRenderer();
+        renderer.setSeriesPaint(0, Color.RED);
+        renderer.setSeriesStroke(0, new BasicStroke(2.0f));
+
+        renderer.setSeriesPaint(1, Color.BLUE);
+        renderer.setSeriesStroke(1, new BasicStroke(2.0f));
+        plot.setRenderer(renderer);
+        plot.setBackgroundPaint(Color.white);
+        plot.setRangeGridlinesVisible(false);
+        plot.setDomainGridlinesVisible(false);
+
+        chart.getLegend().setFrame(BlockBorder.NONE);
+        chart.setTitle(new TextTitle("Зависимость времени выполнения от зума прорисовки", new Font("Serif", Font.BOLD, 18)));
+        ChartUtils.saveChartAsPNG(new File("charts/zoom_chart.png"), chart, 1080, 720);
+
+        JFrame frame = new JFrame("Mandelbrot Fractal");
+        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+        chartPanel.setBackground(Color.white);
+        frame.add(chartPanel);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        while (true) {
+            Thread.sleep(1000);
+        }
+    }
+
+    private static XYSeriesCollection getDataset(List<Number> xMatrix, List<Long> sequentialResults, List<Long> parallelResults) {
+        XYSeries sequentialSeries = new XYSeries("Последовательно");
+        for (int i = 0; i < sequentialResults.size(); i++) {
+            sequentialSeries.add(xMatrix.get(i), sequentialResults.get(i));
+        }
+        XYSeries parallelSeries = new XYSeries("Параллельно");
+        for (int i = 0; i < parallelResults.size(); i++) {
+            parallelSeries.add(xMatrix.get(i), parallelResults.get(i));
+        }
+        XYSeriesCollection dataset = new XYSeriesCollection();
+        dataset.addSeries(sequentialSeries);
+        dataset.addSeries(parallelSeries);
+        return dataset;
     }
 }
